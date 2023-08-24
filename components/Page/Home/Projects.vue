e67dfedb7c15abf6724da822454436967eb1f41c
Bene-volent <benevolent733@gmail.com>
2023-08-24 11:39:08 +0530

    HH

--- components/Page/Home/Projects.vue
+++ components/Page/Home/Projects.vue
@@ -1,8 +1,8 @@
 <template>
     <PageSectionContainer class="projects">
         <div class="projects-headContent">
-            <h2 class="section-heading">Projects</h2>
-            <p class="projects-subtitle">These are some <span class="[ italic bold ]">Frontend Projects</span> that give an
+            <h2 class="section-heading">Featured Works</h2>
+            <p class="projects-subtitle">These are some of my favourite <span class="[ italic bold ]">Frontend Projects</span> that give an
                 idea about my attention to detail, my
                 current skills and tools I use to create such projects.
                 You can read about why and how these projects are made in details by click the explain button on each
